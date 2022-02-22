3d5ef8478ff005ea533723e69a58c8cdc6a947bc
Kalepson <kalepson@gmail.com>
2022-02-22 14:15:19 +0200

    Modificari button disabled

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,13 +1,16 @@
 const buttons = document.querySelectorAll('.btn');
 const container = document.querySelector('.container');
-const form = document.querySelector("#form")
-const nomeLogin = form.querySelector(".nome-login");
-const emailLogin = form.querySelector(".email-login");
-const passwordLogin = form.querySelector(".password-login");
+const formRegistration = document.querySelector("#form")
+const formConnecting = document.querySelector(".form")
+const nomeLogin = formRegistration.querySelector(".nome-login");
+const emailLogin = formRegistration.querySelector(".email-login");
+const passwordLogin = formRegistration.querySelector(".password-login");
 const emailConnecting = document.querySelector(".email-connecting");
 const passwordConnecting = document.querySelector(".password-connecting");
 const validEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 const btnIn = document.querySelector(".sign-In")
+const btnUp = document.querySelector(".sign-Up")
+
 
 const active = () => {
     buttons.forEach((btn) => {
@@ -18,9 +21,9 @@ const active = () => {
 }
 active()
 
+
 const errorName = document.querySelector(".errorName")
 nomeLogin.addEventListener("input", (e) => {
-    const errorName = document.querySelector(".errorName")
     if (nomeLogin.value === "") {
         errorName.textContent = "This field should not be empty"
     } else if (nomeLogin.value.length < 4 || nomeLogin.value.length > 12) {
@@ -33,8 +36,8 @@ nomeLogin.addEventListener("input", (e) => {
 })
 
 
+const errorEmail = document.querySelector(".errorEmail")
 emailLogin.addEventListener('input', () => {
-    const errorEmail = document.querySelector(".errorEmail")
     if (emailLogin.value === "") {
         errorEmail.textContent = "This field should not be empty"
     } else if (!validEmail.test(emailLogin.value)) {
@@ -47,8 +50,8 @@ emailLogin.addEventListener('input', () => {
 })
 
 
+const errorPassword = document.querySelector(".errorPassword")
 passwordLogin.addEventListener('input', () => {
-    const errorPassword = document.querySelector(".errorPassword")
     if (passwordLogin.value === null || passwordLogin.value === "") {
         errorPassword.textContent = "This field should not be empty"
     } else if (passwordLogin.value.length < 6) {
@@ -60,35 +63,62 @@ passwordLogin.addEventListener('input', () => {
     }
 })
 
+
+const btnUpDisabled = () => {
+    btnUp.classList.toggle("disabled")
+  formRegistration.addEventListener("input", () => {
+        if (nomeLogin.value === "") {
+            btnUp.setAttribute("disabled", "disabled")
+        } else if (nomeLogin.value.length < 4 || nomeLogin.value.length > 12) {
+            btnUp.setAttribute("disabled", "disabled")
+        } else if (emailLogin.value === "") {
+            btnUp.setAttribute("disabled", "disabled")
+        } else if (!validEmail.test(emailLogin.value)) {
+            btnUp.setAttribute("disabled", "disabled")
+        } else if (passwordLogin.value === "") {
+            btnUp.setAttribute("disabled", "disabled")
+        } else if (passwordLogin.value.length < 6) {
+            btnUp.setAttribute("disabled", "disabled")
+        } else {
+            btnUp.removeAttribute("disabled")
+            btnUp.classList.remove("disabled")
+        }
+    })
+}
+btnUpDisabled()
+
+
+const error_Email = document.querySelector(".error-Email")
 emailConnecting.addEventListener('input', () => {
-    const errorEmail = document.querySelector(".error-Email")
+    // const errorEmail = document.querySelector(".error-Email")
     if (emailConnecting.value === null || emailConnecting.value === "") {
-        errorEmail.textContent = "This field should not be empty"
+        error_Email.textContent = "This field should not be empty"
     } else if (!validEmail.test(emailConnecting.value)) {
-        errorEmail.textContent = "Please provide a valid email address"
+        error_Email.textContent = "Please provide a valid email address"
         emailConnecting.className = "invalid"
     } else {
         emailConnecting.className = "valid"
-        errorEmail.textContent = ""
+        error_Email.textContent = ""
     }
 })
 
+
+const error_Password = document.querySelector(".error-Password")
 passwordConnecting.addEventListener('input', () => {
-    const errorPassword = document.querySelector(".error-Password")
     if (passwordConnecting.value === "") {
-        errorPassword.textContent = "This field should not be empty"
+        error_Password.textContent = "This field should not be empty"
     } else if (passwordConnecting.value.length < 6) {
-        errorPassword.textContent = "Password must be at least 6 characters"
+        error_Password.textContent = "Password must be at least 6 characters"
         passwordConnecting.className = "invalid"
     } else {
         passwordConnecting.className = "valid"
-        errorPassword.textContent = "";
+        error_Password.textContent = "";
     }
 })
 
 
 let userArray = []
-form.addEventListener('submit', (e) => {
+formRegistration.addEventListener('submit', (e) => {
     const addUser = () => {
         let user = {
             id: Date.now(),
@@ -107,16 +137,34 @@ form.addEventListener('submit', (e) => {
             emailLogin.classList.remove("valid")
             passwordLogin.classList.remove("valid")
             alert("you have successfully registered")
+            btnUp.setAttribute("disabled", "disabled")
+            btnUp.classList.toggle("disabled")
         }
         console.log('contacte', {userArray})
     }
     addUser()
 })
 
-btnIn.addEventListener("click", () => {
+
+btnIn.classList.toggle("disabled")
+formConnecting.addEventListener("input", () => {
+    if (emailConnecting.value === "" && !validEmail.test(emailConnecting.value)) {
+        btnIn.setAttribute("disabled", "disabled")
+    } else if (passwordConnecting.value === "" && passwordConnecting.value.length < 6) {
+        btnIn.setAttribute("disabled", "disabled")
+    } else {
+        btnIn.removeAttribute("disabled")
+        btnIn.classList.remove("disabled")
+    }
+})
+
+
+formConnecting.addEventListener("submit", () => {
     userArray.filter((user) => {
         if (passwordConnecting.value === user.password && emailConnecting.value === user.email) {
             alert("this account is connected")
+            btnIn.setAttribute("disabled", "disabled")
+            btnIn.classList.toggle("disabled")
         } else if (passwordConnecting.value === '' || emailConnecting.value === '') {
             alert("This field should not be empty")
         } else {
